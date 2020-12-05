41391c98878ba6f064fd903fb8fecd48dce22eb0
Angelo Reis <angelo.reis@kumulus.com.br>
2020-12-05 18:12:02 -0300

    fix

--- pages/contact.js
+++ pages/contact.js
@@ -4,6 +4,9 @@ const Contact = () => {
   return (
     <div>
       <h1>Contato</h1>
+      <p>
+        TEXTO QUALQUER: <br/>As experiências acumuladas demonstram que a hegemonia do ambiente político estimula a padronização dos níveis de motivação departamental.
+      </p>
     </div>
   );
 };
