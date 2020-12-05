b4bdc018ddc00ee635c9e415e44fe03bde6663d9
Angelo Reis <angelo.reis@kumulus.com.br>
2020-12-05 18:14:18 -0300

    fix

--- pages/contact.js
+++ pages/contact.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contato</h1>
-      <p>
+    <div className="pt-6">
+      <span className="text-center font-bold my-4 text-2xl">Contato</span>
+      <p className="text-center my-6">
         TEXTO QUALQUER: <br/>As experiências acumuladas demonstram que a hegemonia do ambiente político estimula a padronização dos níveis de motivação departamental.
       </p>
     </div>
